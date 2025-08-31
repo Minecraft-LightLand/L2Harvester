a269c5ff4e739ea0ea7c32815d16ebb3633420c0
lcy0x1 <chengyilin@umail.ucsb.edu>
2025-08-31 23:02:39 +0800

    toml fix

--- src/main/java/dev/xkmc/l2harvester60x/init/L2Harvester60x.java
+++ src/main/java/dev/xkmc/l2harvester60x/init/L2Harvester60x.java
@@ -7,7 +7,7 @@ import org.slf4j.Logger;
 @Mod(L2Harvester60x.MODID)
 public class L2Harvester60x {
 
-	public static final String MODID = "l2harvester05x";
+	public static final String MODID = "l2harvester60x";
 	public static final Logger LOGGER = LogUtils.getLogger();
 
 	public L2Harvester60x() {
